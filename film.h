292d5882d14eaa4ecda736e0e4c75a46f48bde63
Bowei Zhao <bowei@vt.edu>
2016-02-18 21:19:34 -0500

    Update on film.h for backup

--- film.h
+++ film.h
@@ -6,3 +6,46 @@
 // Date: 2/17/2016
 //
 // *-----------------------------------------------* //
+
+#ifndef FILM_H
+#define FILM_H
+
+#include <QDate>
+#include <QDateTime>
+#include <QFile>
+#include <QString>
+#include <QStringList>
+#include <QList>
+#include <QTextStream>
+#include <QDebug>
+#include <QLocale>
+#include <QtAlgorithms>
+#include <QChar>
+
+#include <iostream>
+#include <fstream>
+#include <algorithm> 
+#include <string>
+#include <vector>
+#include <exception>
+#include <stdio.h>
+
+typedef quint64 Length;
+
+class Film
+{
+	public:
+
+	private:
+		QString m_FilmID;
+		QString m_Title;
+		QString m_Director;
+		Length m_FilmLength;
+		QDate m_ReleaseDate;
+
+
+
+};
+
+
+#endif // FILM_H
