62aa5c4ef4bcc2bce472706b4764a86fee1ab547
bowei437 <bowei@vt.edu>
2016-02-18 20:49:22 -0500

    Initial File Upload of Film
    Uploaded the basis files for r1

---
+++ film.cpp
@@ -0,0 +1,8 @@
+////////////////////////////////////////////////////////
+// ECE 3574, Homework 2, Bowei Zhao
+//
+// File name: film.cpp
+// Description: film implementation file
+// Date: 2/17/2016
+//
+// *-----------------------------------------------* //
